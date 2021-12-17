30df36ab84d318223dce630cafe0beb2e7378031
chihaoyo <chihaoyo@gmail.com>
2021-12-17 16:13:37 +0800

    Add

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'arrow-parens': 0,
+    'keyword-spacing': 0,
+    'no-unneeded-ternary': 0,
+    'space-before-function-paren': ['error', 'never'],
+    'vue/comment-directive': 0,
+    'vue/html-indent': 0,
+    'vue/html-self-closing': 0,
+    'vue/no-v-html': 0,
+    'vue/singleline-html-element-content-newline': 0
+  }
+}
